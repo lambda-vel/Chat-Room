6b6eba737205135bfee5360a38aa686a3e864541
Mir Suhail Asarat <suhail.asarat@gmail.com>
2023-03-24 00:37:48 +0600

    updated

--- Server.java
+++ Server.java
@@ -77,26 +77,32 @@ public class Server implements Runnable{
                 in = new BufferedReader(new InputStreamReader(client.getInputStream()));
                 out.println("Please enter a nickname: ");
                 nickname = in.readLine();
-                System.out.println(nickname + " connected!");
-                broadcast(nickname + " joined the chat!");
-                String message;
-                while ((message = in.readLine()) != null){
-                    if (message.startsWith("/nick ")){
-                        String[] messageSplit = message.split(" ", 2);
-                        if (messageSplit.length == 2){
-                            broadcast(nickname + " changed their nickname to " + messageSplit[1]);
-                            nickname = messageSplit[1];
-                            out.println("Successfully changed nickname to " + nickname);
+                if (connections.contains?(nickname)){
+                    out.println("Please enter a currect nickname.");
+                }
+                else{
+                    System.out.println(nickname + " connected!");
+                    broadcast(nickname + " joined the chat!");
+                    String message;
+                    while ((message = in.readLine()) != null){
+                        if (message.startsWith("/nick ")){
+                            String[] messageSplit = message.split(" ", 2);
+                            if (messageSplit.length == 2){
+                                broadcast(nickname + " changed their nickname to " + messageSplit[1]);
+                                nickname = messageSplit[1];
+                                out.println("Successfully changed nickname to " + nickname);
+                            } else {
+                                out.println("No nickname provided!");
+                            }
+                        } else if (message.startsWith("/quit")){
+                            broadcast(nickname + " left the chat!");
+                            shutdown();
                         } else {
-                            out.println("No nickname provided!");
+                            broadcast(nickname + ": " + message);
                         }
-                    } else if (message.startsWith("/quit")){
-                        broadcast(nickname + " left the chat!");
-                        shutdown();
-                    } else {
-                        broadcast(nickname + ": " + message);
                     }
                 }
+                
                 
             } catch (IOException e) {
                 shutdown();
